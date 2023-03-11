c9803b0d31bb321556fbe83fa34afc11e88f06fc
Dovudhon <dovudkhonhusanov@gmail.com>
2023-03-11 16:54:17 +0500

    first commit

---
+++ src/components/config/theme.js
@@ -0,0 +1,16 @@
+export const colors: { [key: string]: string }  = {
+  darkGrey: "9D9FB1",
+  brightGrey: "#F9FAFC",
+  lightGrey: "#FAFBFD",
+  brightViolet: "#5D60EF",
+  opacityViolet: "#F4E8FF",
+  white: "#ffffff",
+  black: "#161D49"
+};
+
+export const shadows = {
+  dropShadow: "1px 2px rgba(54, 58, 69, 0.08)",
+  dramatic: "-31px -22px 19px rgba(0, 0, 0, 0.3)",
+  lightDropShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+  textShadow: "0px 13px 19px rgba(0, 0, 0, 0.24)",
+};
